02a7d519be79c9c61f58210465934ff84b8969ec
classicdanno <DanEliGoldfarb@gmail.com>
2026-05-06 00:03:58 -0400

    proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,1 +1,27 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
 
+  const { endpoint, ...params } = req.query;
+
+  if (!endpoint) {
+    return res.status(400).json({ error: 'Missing endpoint' });
+  }
+
+  const qs = new URLSearchParams(params).toString();
+  const url = `https://pokemon-tcg-api.p.rapidapi.com/${endpoint}${qs ? '?' + qs : ''}`;
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        'x-rapidapi-key': process.env.POKEMON_API_KEY,
+        'x-rapidapi-host': 'pokemon-tcg-api.p.rapidapi.com'
+      }
+    });
+
+    const data = await response.json();
+    return res.status(response.status).json(data);
+  } catch (err) {
+    return res.status(500).json({ error: 'Proxy fetch failed', detail: err.message });
+  }
+}
