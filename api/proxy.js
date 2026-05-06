c06f0d1a83d9e979b878706462a3f3e4182599c9
classicdanno <DanEliGoldfarb@gmail.com>
2026-05-06 00:15:41 -0400

    proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -8,8 +8,11 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: 'Missing endpoint' });
   }
 
+  // Pass key as both header AND query param as the API docs show
+  params['rapidapi-key'] = process.env.POKEMON_API_KEY;
+
   const qs = new URLSearchParams(params).toString();
-  const url = `https://pokemon-tcg-api.p.rapidapi.com/pokemon/${endpoint}${qs ? '?' + qs : ''}`;
+  const url = `https://pokemon-tcg-api.p.rapidapi.com/${endpoint}?${qs}`;
 
   try {
     const response = await fetch(url, {
